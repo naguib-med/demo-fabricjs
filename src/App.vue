474f9cdc55bf1ee92d7e01b071dd9c36b0af390e
MOHAMED MAHAMOUD NAGUIB <naguib.mohamed-mahamoud@etu.univ-lyon1.fr>
2023-04-11 13:07:36 +0200

    demo editor app fabricjs

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+   <v-app>
+      <v-app-bar app color="grey-darken-4">
+         <v-toolbar-title>Poster Editor</v-toolbar-title>
+         <v-spacer></v-spacer>
+         <v-btn color="dark" to="/">Github</v-btn>
+      </v-app-bar>
+      <v-main>
+          <RouterView />
+      </v-main>
+    </v-app>
+</template>
+
